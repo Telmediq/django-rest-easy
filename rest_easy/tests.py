14bc1eee9e15eecf6e4474f48e0df581be323f41
Krzysztof Bujniewicz <racech@gmail.com>
2017-07-06 14:28:44 +0200

    Travis.

---
+++ rest_easy/tests.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import unittest
+
+
+class NullTest(unittest.TestCase):
+    def test_null(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
