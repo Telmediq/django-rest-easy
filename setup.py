c826ec438d602ec68ea290997ea37d83e0fed5ca
Krzysztof Bujniewicz <racech@gmail.com>
2017-07-07 11:45:55 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='django-rest-easy',
     packages=['rest_easy'],
-    version='0.1.1',
+    version='0.1.2',
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     install_requires=[
         'django>=1.8.0',
