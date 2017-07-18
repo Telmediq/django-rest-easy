a5670b54d334b3200c23eb9062a2c3236f017ad3
Krzysztof Bujniewicz <racech@gmail.com>
2017-07-18 14:47:23 +0200

    Fix StaticField docstring.

--- rest_easy/fields.py
+++ rest_easy/fields.py
@@ -14,12 +14,12 @@ class StaticField(Field):  # pylint: disable=abstract-method
     A field that always provides the same value as output.
 
     The output value is set on initialization, ie::
-    
+
         from rest_easy.serializers import Serializer
 
         class MySerializer(Serializer):
             static = StaticField('This will always be the value.')
-    
+
     """
 
     def __init__(self, value, **kwargs):
