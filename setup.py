34927c61b17dd9355360b5c9a750126c950d8256
Krzysztof Bujniewicz <racech@gmail.com>
2017-12-04 16:06:00 +0100

    0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='django-rest-easy',
     packages=['rest_easy'],
-    version='0.2.0',
+    version='0.2.1',
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     install_requires=[
         'django>=1.8.0',
@@ -12,10 +12,10 @@ setup(
         'six',
     ],
     description='django-rest-easy is an extension to DRF providing QOL improvements to serializers and views.',
-    long_description='django-rest-easy enables:'
-                     ' * versioning serializers by model and schema,'
-                     ' * creating views and viewsets using model and schema,'
-                     ' * serializer override for a particular DRF verb, like create or update,'
+    long_description='django-rest-easy enables:\n'
+                     ' * versioning serializers by model and schema,\n'
+                     ' * creating views and viewsets using model and schema,\n'
+                     ' * serializer override for a particular DRF verb, like create or update,\n'
                      ' * scoping views\' querysets and viewsets by url kwargs or request object parameters.',
     author='SMARTPAGER SYSTEMS INC. / Krzysztof Bujniewicz',
     author_email='racech@gmail.com',
@@ -28,7 +28,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
